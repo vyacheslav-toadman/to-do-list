03b016e8f2751e4e9fad34a35c03285923af11eb
Vyacheslav Nikolaev <vyacheslav.nikolaev@thetoadman.com>
2022-01-10 16:08:24 +0300

    Create "to do list" starter template based omn Angular
    Add base markup with no styles, implement simple angular logic for new todo items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'to-do-list';
+
+  filter: 'all' | 'active' | 'done' = 'all';
+
+  allItems = [
+    { description: 'breakfast',
+      done: true
+    },
+    { description: 'brush teeth',
+      done: true
+    },
+    { description: 'homework',
+      done: false
+    },
+    { description: 'dress',
+      done: true
+    },
+  ];
+
+  get items() {
+    if(this.filter === 'all') {
+      return this.allItems;
+    }
+    return this.allItems.filter(item => this.filter === 'done' ? item.done : !item.done);
+  }
+
+  addItem(description: string) {
+    this.allItems.unshift({
+      description,
+      done: false,
+    });
+  }
 }
